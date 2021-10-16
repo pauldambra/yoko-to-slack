9668b366a160b332decd8fd82d94572789017656
Paul D'Ambra <paul.dambra@gmail.com>
2021-10-16 01:54:20 +0100

    without permission to detect labels

--- slurp-to-labelling/handler.ts
+++ slurp-to-labelling/handler.ts
@@ -1,8 +1,33 @@
-import { SQSEvent } from "aws-lambda";
+import { SQSEvent } from 'aws-lambda'
+
+import Rekognition, { DetectLabelsRequest } from 'aws-sdk/clients/rekognition'
+import axios from 'axios'
+
+const rekognition = new Rekognition()
+
+const getFileContentById = async (
+  download_url: string
+): Promise<Buffer> => {
+  const response = await axios.get(download_url, {
+    responseType: 'arraybuffer'
+  })
+  return Buffer.from((response.data as string), 'base64')
+}
 
 export const run = async (event: SQSEvent) => {
-  event.Records.forEach(r => {
-    console.log({ r })
-  })
+  const labels = await Promise.all(event.Records.map(r => JSON.parse(r.body)).map(toot => {
+    const mediaUrl = toot.entities.media[0].media_url
+    return getFileContentById(mediaUrl).then(buffer => {
+      console.log({ mediaUrl, buffer, e: toot.entities, m: toot.entities.media })
+      const dlp: DetectLabelsRequest = {
+        Image: {
+          Bytes: buffer
+        }
+      }
+      return rekognition.detectLabels(dlp).promise()
+    })
+
+  }))
+  console.log(labels)
   throw new Error('no processing yet')
 }
